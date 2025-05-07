2c9c8a8e455b9571e4f0d67098e34fe0cbd55c87
OrgesSadriu <gesi.sadriu@gmail.com>
2025-05-07 03:32:55 +0200

    testing production

--- client/src/environment/environment.ts
+++ client/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: "http://localhost:5000",
-  prodUrl: "https://lab-project-2.onrender.com",
+  prodUrl: "https://taxi-kdar.onrender.com",
 };
